e4a2d3a99f34d1038caeff07b8ed35dccf35de8d
bertrandp <bertrand.pestre@gmail.com>
2018-08-06 19:48:51 +0200

    Use promises in validateDiscount instead of synchronous methods

--- src/services/promocode.js
+++ src/services/promocode.js
@@ -1,82 +1,75 @@
 const weatherClient = require('./../clients/weather')
 const moment = require('moment')
+var Promise = require("bluebird");
 
 module.exports = {
     
     validateDiscount: (promocode, arguments) => {
-        return weatherClient.get(arguments.meteo.town)
-          .then(weather => {
-            const validateAll = (restrictions) => {
-                return restrictions.every(restriction => evaluateNode(restriction))
-            }
-            
-            const validateAny = (restrictions) => {
-                return restrictions.some(restriction => evaluateNode(restriction))
-            }
-    
-            const evaluateNode = (restriction) => {
-                if (restriction.name === 'meteo') {
-                    return validateWeather(restriction.data)
-                } else if (restriction.name === 'date') {
-                    return validateDate(restriction.data)
-                } else if (restriction.name === 'age') {
-                    return validateAge(restriction.data)
-                } else if (restriction.name === 'or') {
-                    return validateAny(restriction.restrictions)
-                } else if (restriction.name === 'and') {
-                    return validateAll(restriction.restrictions)
-                }
-            }
-    
-            const validateWeather = (data) => {
-                if(weather.temp > data.temp.gt && weather.is.toUpperCase() === data.is.toUpperCase()) {
-                    return true
-                }
-                errors.push('Temperature must be above ' + data.temp.gt + ' (' + weather.temp + ') and weather must be ' + data.is + ' but it\'s ' + weather.is)
-                return false;
-            }
-    
-            const validateDate = (data) => {
-                const currentDate = moment()
-                if(currentDate > moment(data.after) && currentDate < moment(data.before)) {
-                    return true
-                }
-                errors.push('Date must be between ' + data.after + ' and ' + data.before)
-                return false
-            }
-    
-            const validateAge =  (data) => {
-                if(!arguments.age) {
-                    return false
-                } else if (data.eq) {
-                    if(data.eq === arguments.age) {
-                        return true
-                    }
-                    errors.push('Age not equal ' + data.eq)
-                    return false
-                } else if (data.lt && data.gt) {
-                    if(arguments.age < data.lt && arguments.age > data.gt) {
-                        return true
-                    }
-                    errors.push('Age must be between ' + data.lt + ' and ' + data.gt)
-                    return false
-                }
-                return false;
-            }
-    
-            const errors = []
 
-            result = validateAll(promocode.restrictions)
+        const validateAll = (restrictions, arguments) => {
+            return Promise.all(restrictions.map(restriction => evaluateNode(restriction, arguments)))
+        }
 
-            return result ? {
-                promocode_name: promocode.name, 
-                status: 'accecpted',
-                avantage: promocode.avantage
-            } : {
-                promocode_name: promocode.name, 
-                status: 'denied',
-                reasons: errors
+        const validateAny = (restrictions, arguments) => {
+            return Promise.any(restrictions.map(restriction => evaluateNode(restriction, arguments)))
+        }
+
+        const evaluateNode = (restriction, arguments) => {
+            if (restriction.name === 'meteo') {
+                return validateWeather(restriction.data, arguments)
+            } else if (restriction.name === 'date') {
+                return validateDate(restriction.data)
+            } else if (restriction.name === 'age') {
+                return validateAge(restriction.data, arguments)
+            } else if (restriction.name === 'or') {
+                return validateAny(restriction.restrictions, arguments)
+            } else if (restriction.name === 'and') {
+                return validateAll(restriction.restrictions, arguments)
+            }
+        }
+
+        const validateWeather = (data, arguments) => {
+            return weatherClient.get(arguments.meteo.town)
+                .then(weather => {
+                    if(weather.temp < data.temp.gt || weather.is.toUpperCase() !== data.is.toUpperCase()) {
+                        throw 'Temperature must be above ' + data.temp.gt + ' (' + weather.temp + ') and weather must be ' + data.is + ' but it\'s ' + weather.is;
+                    }
+                })
+        }
+
+        const validateDate = Promise.method((data) => {
+            const currentDate = moment()
+            if(currentDate < moment(data.after) || currentDate > moment(data.before)) {
+                throw 'Date must be between ' + data.after + ' and ' + data.before
             }
         })
+
+        const validateAge =  Promise.method((data, arguments) => {
+            if (data.eq) {
+                if(data.eq !== arguments.age) {
+                    throw 'Age must be equal to ' + data.eq
+                }
+            } else if (data.lt && data.gt) {
+                if(arguments.age > data.lt || arguments.age < data.gt) {
+                    throw 'Age must be between ' + data.lt + ' and ' + data.gt
+                }
+            }
+        })
+
+        return validateAll(promocode.restrictions, arguments)
+            .then(() => {
+                return {
+                    promocode_name: promocode.name,
+                    status: 'accecpted',
+                    avantage: promocode.avantage
+                }
+            })
+            .catch(error => {
+                return {
+                    promocode_name: promocode.name, 
+                    status: 'denied',
+                    reasons: error
+                }
+            })
     }
 }
